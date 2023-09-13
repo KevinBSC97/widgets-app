80c34fad87fd3f953fa17560e1a7f05f8a8e54c9
KevinBSC97 <kevinsotomayor1@hotmail.com>
2023-09-13 16:04:11 -0500

    theme changer screen creada

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -69,4 +69,10 @@ const appMenuItems = <MenuItem>[
     link: '/counter-river', 
     icon: Icons.add
   ),
+  MenuItem(
+    title: 'Cambiar Tema', 
+    subtitle: 'Cambiar tema de la aplicación', 
+    link: '/theme-changer', 
+    icon: Icons.color_lens_outlined
+  ),
 ];
--- lib/presentation/providers/theme_provider.dart
+++ lib/presentation/providers/theme_provider.dart
@@ -1,3 +1,7 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:widgets_app/config/theme/app_theme.dart';
 
 final isDarkModeProvider = StateProvider((ref) => false);
+
+//Listado de colores inmutables
+final colorListProvider = Provider((ref) => colorList);
